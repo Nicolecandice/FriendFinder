c64817d917ac7d6544dd5119b36328c69896f354
nicolecandice <nicolecoope@gmail.com>
2018-07-31 00:57:18 -0400

    add photo links

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,41 +1,109 @@
+// ===============================================================================
 // DATA
-// Below data will hold all of the friends tables.
-// Initially we just set it equal to "dummy" customers.
-
+// Below data will hold all of the possible friends.
 // ===============================================================================
 
-var friends = [{
-    name: "Charles Smith",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-},
-{
 
-    name: "Sam Thomas",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-},
-
-{
-    name: "Mark Wells",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-},
-{
-
-    name: "Jake Sleepy",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-},
-
-
-{
-    name: "Candy",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [3, 1, 4, 3, 5, 1, 4, 5, 4, 2]
-}];
-
-
-
-// Note how we export the array. This makes it accessible to other files using require.
-module.exports = friends;
+var friends = [
+    {
+      name: "Carl Johnson",
+      photo: "https://thoughtcatalog.files.wordpress.com/2017/03/img_1305.png?w=800&h=1200",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Paul Smith",
+      photo: "http://www.wuft.org/news/files/2014/09/Kyle-Leonard.jpg",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Jacob Charles",
+      photo: "http://1.bp.blogspot.com/-1SaJ4EpP9RY/T9pSgQn2TPI/AAAAAAAAAnY/XwI9rtb9ZTs/s1600/Sabir-3.jpg",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Brain Kidding",
+      photo: "http://mybrownbaby.com/wp-content/uploads/2012/01/taye-diggs-profile.jpg",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Sam Thomas",
+      photo: "https://www.nbr.co.nz/sites/default/files/styles/article_full_300w/public/story_imgs/WilliamEaston1.png?itok=JyU2JZsN ",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Mike Mitchell",
+      photo: " https://i.ytimg.com/vi/SKcIGZDEXZY/hqdefault.jpg",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    }
+  
+  ];
+  
+  // Here we export the array. This makes it accessible to other files using require.
+  module.exports = friends;
+  
